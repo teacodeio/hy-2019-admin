6b115270df08f96c31616c6c7dcd416a8bcb2d1f
Michal Pierzchlewicz <mike@teacode.io>
2019-09-14 13:57:04 +0200

    init setup

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Admin, Resource } from 'react-admin'
+import { authClient } from 'ra-data-feathers'
+import englishMessages from 'ra-language-english'
+
+import Person from '@material-ui/icons/Person'
+
+import feathersRestClient from './client/feathersRestClient'
+import createRealtimeSaga from './redux/sagas/createRealtimeSaga'
+import domainMessages from './i18n'
+import { defaultTheme } from './themes'
+
+import dataProvider from './dataProvider'
+
+import { AdminCreate, AdminEdit, AdminList } from './models/admins'
+
+const authClientOptions = {
+  storageKey: 'feathers-jwt', // The key in localStorage used to store the authentication token
+  authenticate: { // Options included in calls to Feathers client.authenticate
+    strategy: 'admin' // The authentication strategy Feathers should use
+  },
+  permissionsKey: 'permissions', // The key in localStorage used to store permissions from decoded JWT
+  permissionsField: 'roles', // The key in the decoded JWT containing the user's role
+  passwordField: 'password', // The key used to provide the password to Feathers client.authenticate
+  usernameField: 'email', // The key used to provide the username to Feathers client.authenticate
+  redirectTo: '/login' // Redirect to this path if an AUTH_CHECK fails. Uses the react-admin default of '/login' if omitted.
+}
+
+const realTimeSaga = createRealtimeSaga(dataProvider)
+
+const messages = {
+  en: {
+    ...englishMessages,
+    ...domainMessages.en
+  }
+}
+
+const i18nProvider = locale => messages[locale]
+
+const App = () =>
+  <Admin
+    title='Butt buster: Admin panel'
+    dataProvider={dataProvider}
+    authProvider={authClient(feathersRestClient, authClientOptions)}
+    customSagas={[realTimeSaga]}
+    locale='en'
+    i18nProvider={i18nProvider}
+    theme={defaultTheme}
+  >
+    <Resource
+      name={'admins'}
+      icon={Person}
+      list={AdminList}
+      create={AdminCreate}
+      edit={AdminEdit}
+    />
+  </Admin>
+
+export default App
---
+++ src/models/admins/index.js
@@ -0,0 +1,105 @@
+import React from 'react'
+
+import {
+  List,
+  DateField,
+  TextField,
+  Filter,
+  SearchInput,
+  Create,
+  SimpleForm,
+  TextInput,
+  Edit,
+  CardActions,
+  ListButton,
+  TabbedForm,
+  FormTab,
+  required,
+  Datagrid
+} from 'react-admin'
+import DateFilters from '../../elements/DateFilters'
+import DuplicateButton from '../../elements/DuplicateButton'
+
+export const Filters = (props) => (
+  <Filter {...props}>
+    <SearchInput
+      label='Email'
+      source={`email.$regex`}
+      alwaysOn
+    />
+
+    {DateFilters}
+  </Filter>
+)
+
+const CreateActions = ({ basePath }) => (
+  <CardActions>
+    <ListButton basePath={basePath} />
+  </CardActions>
+)
+
+const EditActions = ({ basePath, data }) => (
+  <CardActions>
+    <ListButton basePath={basePath} />
+    <DuplicateButton basePath={basePath} record={data} />
+  </CardActions>
+)
+
+export const AdminList = props => (
+  <List
+    {...props}
+    filters={<Filters />}
+  >
+    <Datagrid
+      rowClick='edit'
+    >
+      <TextField source='email' />
+      <DateField source='createdAt' />
+      <DateField source='updatedAt' />
+    </Datagrid>
+  </List>
+)
+
+export const AdminCreate = (props) => (
+  <Create
+    {...props}
+    actions={<CreateActions />}
+  >
+    <SimpleForm>
+      <TextInput
+        source={'email'}
+        type='email'
+        validate={required()}
+      />
+      <TextInput
+        source={'password'}
+        type='password'
+        validate={required()}
+      />
+    </SimpleForm>
+  </Create>
+)
+
+export const AdminEdit = (props) => (
+  <Edit
+    {...props}
+    actions={<EditActions />}
+  >
+    <TabbedForm redirect={false}>
+      <FormTab replace label='summary'>
+        <TextField source='id' />
+        <TextInput
+          source={'email'}
+          type='email'
+          validate={required()}
+        />
+        <TextInput
+          source={'password'}
+          type='password'
+        />
+        <DateField source='createdAt' showTime />
+        <DateField source='updatedAt' showTime />
+      </FormTab>
+    </TabbedForm>
+  </Edit>
+)
